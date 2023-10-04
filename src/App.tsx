273492cb7d87e49ef2d3fa7198c12e55cc1ac706
annavancity <anna.designers@yahoo.com>
2023-10-04 10:38:35 -0700

    code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Board from "./Board";
 
-function App() {
+const App: React.FC = () => {
+    const initialTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+    const [tiles, setTiles] = useState(initialTiles);
+    // function shuffle tiles
+    const shuffleTiles = () => {
+        const shuffledTiles = [...initialTiles]; // copy of an array
+        for(let i = shuffledTiles.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random()*(i + 1));
+            [shuffledTiles[i], shuffledTiles[j]] = [shuffledTiles[j], shuffledTiles[i]]; // destructive assignment
+        }
+        setTiles(shuffledTiles);
+    }
+    // function click on tile
+    const handleTileClick = (index: number) => {
+        if(canMoveTile(index)) {
+            const newTiles = [...tiles]; // copy of an array
+            const emptyIndex = newTiles.indexOf(0);
+            [newTiles[index], newTiles[emptyIndex]] = [newTiles[emptyIndex], newTiles[index]];
+            setTiles(newTiles);
+//check if the game is complete, show message
+            if(isGameComplete(newTiles)) {
+                alert('Congratulations!')
+            }
+        }
+    }
+    // function moving tile to empty index
+    const canMoveTile = (index: number) => {
+        const emptyIndex = tiles.indexOf(0);
+        const row = Math.floor(emptyIndex / 4);
+
+        return (
+            //up
+            (index === emptyIndex - 4 && row > 0) ||
+            //down
+            (index === emptyIndex - 4 && row < 3) ||
+             //right (checking if we can move tile to the left within boundaries on the left)
+            (index === emptyIndex - 1 && row % 4 !== 3) ||
+            //left
+            (index === emptyIndex + 1 && row % 4 !== 0)
+        )
+    }
+
+    //function to check if the game is complete
+    const isGameComplete = (currentTiles: number[]) => {
+        for(let i = 0; i < currentTiles.length -1; i++){
+            if(currentTiles[i] !== i + 1) {
+                return false; // if the tile is not on a right spot
+            }
+            return true;
+        }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>15 Puzzle</h1>
+        < Board
+            tiles = {tiles}
+            onTileClick = {handleTileClick}
+        />
+        <button onClick={shuffleTiles}>Shuffle</button>
+
     </div>
   );
 }
---
+++ src/Board.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tile from "./Tile";
+interface BoardProps {
+    tiles: number[];
+    onTileClick: (index: number) => void;
+}
+const Board: React.FC<BoardProps> = ({tiles, onTileClick}) => {
+    const renderRow = (rowIndex: number) => {
+        return (
+            <div key={rowIndex} className="board-row">
+                {tiles.slice(rowIndex * 4, (rowIndex +1) * 4).map((value, index) => (
+                    < Tile key={index}
+                           value={value}
+                           onClick = {() => onTileClick(index + rowIndex * 4)}
+            />
+                    ))}
+            </div>
+        )
+    }
+    return <div className="board"> {[0, 1, 2, 3].map(renderRow)} </div>
+};
+
+export default Board;
